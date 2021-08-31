b99d773f8c906e1fce575ecc44d3ea233bc65ee0
ernestprojeckt <87386250+ernestprojeckt@users.noreply.github.com>
2021-08-31 18:37:32 +0300

    Add files via upload

---
+++ HomeworckCC/common/Database/Slect.php
@@ -0,0 +1,19 @@
+<?php
+
+//'SELECT Galerry.id as gallery_id, post.id as post_id, Galerry.title as gallery_title, post.title as post_title FROM Galerry INNER JOIN post ON Galerry.id = post.galerry_id WHERE post.id = 2 OR post.id = 3 order by post.id DESC ';
+namespace Common\Database;
+
+
+class Slect
+{
+    public $nameId;
+    public $;
+    public $postid;
+    public
+
+    public function Slect()
+    {
+
+
+    }
+}
